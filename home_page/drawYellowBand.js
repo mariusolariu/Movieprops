ab512ab27c6487cc2d27b67338a50ffddcf9fd03
Marius <marius.lucian.olariu@gmail.com>
2018-11-11 16:48:47 +0000

    start

---
+++ home_page/drawYellowBand.js
@@ -0,0 +1,27 @@
+			window.addEventListener("load", main, false);
+
+			function main(){
+				var context = document.getElementById("drawMottoBackground").getContext("2d");
+				var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+				var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+				
+				var gradient = context.createLinearGradient(0,0,0,125);
+				gradient.addColorStop(0, "yellow");
+				gradient.addColorStop(0.3, "orange");
+				gradient.addColorStop(0.7, "orange");
+				gradient.addColorStop(1, "yellow");
+
+				context.fillStyle = gradient;
+				context.fillRect(0,0, width, height );
+
+				
+				//write the motto
+			/*	context.font = "bold 30px sans-serif";
+				context.textAlign = "center";
+				context.strokeStyle = "navy";
+				context.fillStyle = "red";
+				context.lineWidth = 2;
+				context.strokeText("Bring movies to life!", 0, 0);*/
+			}
+	
