9e6f2165dada79288c6216a6edbd8d8a432ada3e
Marius <marius.lucian.olariu@gmail.com>
2018-11-29 01:11:33 +0000

    update

---
+++ registration/switchContent.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", main, false);
+
+function $(x) { return document.getElementById(x); }
+
+var loginDiv;
+var newAccFormDiv;
+
+function main(){
+	loginDiv = $("loginDiv");
+	newAccFormDiv = $("newAccFormDiv");	
+
+	$("newAccA").addEventListener("click", changeContent, false);
+	$("goBackButton").addEventListener("click", goBackMainPage, false);
+}
+
+function changeContent(){
+	loginDiv.style.display = "none";
+	newAccFormDiv.style.display = "block";
+}
+
+function goBackMainPage(){
+	loginDiv.style.display = "block";
+	newAccFormDiv.style.display = "none";
+}
+
